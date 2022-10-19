a8c43aa2e1cb1a0ae95878d7b62669d6cbe88e21
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2022-10-19 15:01:30 -0300

    calculando o ticket medio por loja

--- main.py
+++ main.py
@@ -3,19 +3,46 @@ import pandas as pd
 # Configurações iniciais
 pd.set_option('display.max_columns', None)  # Mostrar todas as colunas
 
+
+def importar_dados(path):
+    return pd.read_excel(path)
+
+
+def visualizar_dados(dados):
+    print(dados.head())
+    return None
+
+
+def calcular_faturamento(dados):
+    return dados[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+
+
+def calcular_quantidade_vendida(dados):
+    return dados[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+
+
+def calcular_ticket_medio(faturamento, quantidade):
+    ticket = faturamento['Valor Final'] / quantidade['Quantidade']
+
+    # transformar em tabela
+    return ticket.to_frame()
+
+
 # importar dados
-dados = pd.read_excel('Vendas.xlsx')
+arquivo = 'Vendas.xlsx'
+dados = importar_dados(arquivo)
 
 # visualizar base de dados
-# print(dados.head())
+# visualizar_dados(dados)
 
 # Faturamento por loja
-faturamento = dados[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+faturamento = calcular_faturamento(dados)
 
 # Quantidade de produtos vendidos por loja
-quantidade = dados[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+quantidade = calcular_quantidade_vendida(dados)
 
 # Ticket médio por produto em cada loja (Fatuamento / quantidade)
-
+ticket_medio = calcular_ticket_medio(faturamento, quantidade)
+visualizar_dados(ticket_medio)
 
 # Enviar um e-mail com o relatorio
